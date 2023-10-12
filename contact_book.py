31ac710914201ca67159043242e25e0801bb4311
Raul de Luca <rauldeveloperproject1447@gmail.com>
2023-10-11 22:34:52 -0300

    Dia 2
    Deletar adicionado. Tratamento de exceção

--- contact_book.py
+++ contact_book.py
@@ -1,17 +1,18 @@
 import sqlite3
 
-conn = sqlite3.connect('contactBook.db')
-c = conn.cursor()
-
 #Auto Increment Só funciona quando é escrito integer primary key, devido a um bug da dependência
-
-c.execute("""CREATE TABLE if not exists contacts(
-            id INTEGER PRIMARY KEY,
-            first_name text not null,
-            last_name text not null,
-            address text not null,
-            contact_number integer not null
-            )""")
+def create_database():
+    conn = sqlite3.connect('contactBook.db')
+    c = conn.cursor()
+    c.execute("""CREATE TABLE if not exists contacts(
+                id INTEGER PRIMARY KEY,
+                first_name text not null,
+                last_name text not null,
+                address text not null,
+                contact_number integer not null
+                )""")
+    conn.commit()
+    conn.close()
 
 def add_contact():
     first_name_input = input("\nInsert the First Name: ")
@@ -41,37 +42,41 @@ def search_contact():
     pending_response = True
 
     while pending_response:
-        choice = int(input("\n1 - First Name\n2 - Last Name\n3 - Address\n" +
-                                "4 - Contact Number\n5 - Return\nChoice: "))
+        try:
+            choice = int(input("\n1 - First Name\n2 - Last Name\n3 - Address\n" +
+                                    "4 - Contact Number\n5 - Return\nChoice: "))
 
-        if choice == 1:
-            search = input("\nInsert the First Name That You Want to Search: ")
-            c.execute("SELECT * FROM contacts WHERE first_name = '{}'".format(search))
-            lister()
-            pending_response = False
+            if choice == 1:
+                search = input("\nInsert the First Name That You Want to Search: ")
+                c.execute("SELECT * FROM contacts WHERE first_name = '{}'".format(search))
+                lister()
+                pending_response = False
 
-        elif choice == 2:
-            search = input("\nInsert the Last Name That You Want to Search: ")
-            c.execute("SELECT * FROM contacts WHERE last_name = '{}'".format(search))
-            lister()
-            pending_response = False
+            elif choice == 2:
+                search = input("\nInsert the Last Name That You Want to Search: ")
+                c.execute("SELECT * FROM contacts WHERE last_name = '{}'".format(search))
+                lister()
+                pending_response = False
 
-        elif choice == 3:
-            search = input("\nInsert the Address That You Want to Search: ")
-            c.execute("SELECT * FROM contacts WHERE address = '{}'".format(search))
-            lister()
-            pending_response = False
+            elif choice == 3:
+                search = input("\nInsert the Address That You Want to Search: ")
+                c.execute("SELECT * FROM contacts WHERE address = '{}'".format(search))
+                lister()
+                pending_response = False
 
-        elif choice == 4:
-            search = input("\nInsert the Contact That You Want to Search: ")
-            c.execute("SELECT * FROM contacts WHERE contact_number = '{}'".format(search))
-            lister()
-            pending_response = False
+            elif choice == 4:
+                search = input("\nInsert the Contact That You Want to Search: ")
+                c.execute("SELECT * FROM contacts WHERE contact_number = '{}'".format(search))
+                lister()
+                pending_response = False
 
-        elif choice == 5:
-            pending_response = False
-        else:
-            print("\nInvalid Choice! Try Again\n")
+            elif choice == 5:
+                pending_response = False
+            else:
+                print("\nInvalid Choice! Try Again\n")
+
+        except ValueError:
+            print("\nChoose an Option")
 
     conn.commit()
     conn.close()
@@ -83,6 +88,7 @@ def drop_table():
     c.execute("DROP TABLE contacts")
     conn.commit()
     conn.close()
+    create_database()
 
 def lister():
     global rows, c
@@ -92,65 +98,113 @@ def lister():
         print(row)
 
 def update_contact():
+
     conn = sqlite3.connect('contactBook.db')
     global rows, c
-    c = conn. cursor()
+    c = conn.cursor()
     running = True
     while running:
-        change = int(input("What do you want to change?\n1 - First Name\n2 - Last Name\n3 - Address\n" +
-                           "4 - Contact Number\n5 - Nothing/Return"))
-        if change == 1:
-            alteration = "first_name"
-            search = input("Which First Name do you want to change: ")
-            update = input("\nInsert the First Name: ")
-            running = False
+        try:
+            change = int(input("What do you want to change?\n1 - First Name\n2 - Last Name\n3 - Address\n" +
+                               "4 - Contact Number\n5 - Nothing/Return"))
+            if change == 1:
+                alteration = "first_name"
+                search = input("Which First Name do you want to change: ")
+                update = input("\nInsert the First Name: ")
+                running = False
 
-        elif change == 2:
-            alteration = "last_name"
-            search = input("Which Last Name do you want to change: ")
-            update = input("\nInsert the Last Name: ")
-            running = False
-        elif change == 3:
-            alteration = "address"
-            search = input("Which Address do you want to change: ")
-            update = input("\nInsert the Address: ")
-            running = False
-        elif change == 4:
-            alteration = "contact_number"
-            search = input("Which Contact do you want to change: ")
-            update = input("\nInsert the Contact's Number: ")
-            running = False
-        elif change == 5:
-            running = False
-            pass
-        else:
-            print("Invalid Option! Try Again\n")
-    sql = ('''UPDATE contacts SET "{0}" = "{1}" WHERE "{0}" = "{2}"'''.format(alteration, update, search))
-    c.execute(sql)
-
-
+            elif change == 2:
+                alteration = "last_name"
+                search = input("Which Last Name do you want to change: ")
+                update = input("\nInsert the Last Name: ")
+                running = False
+            elif change == 3:
+                alteration = "address"
+                search = input("Which Address do you want to change: ")
+                update = input("\nInsert the Address: ")
+                running = False
+            elif change == 4:
+                alteration = "contact_number"
+                search = input("Which Contact do you want to change: ")
+                update = input("\nInsert the Contact's Number: ")
+                running = False
+            elif change == 5:
+                running = False
+                pass
+            else:
+                print("Invalid Option! Try Again\n")
+            sql = ('''UPDATE contacts SET "{0}" = "{1}" WHERE "{0}" = "{2}"'''.format(alteration, update, search))
+            c.execute(sql)
+        except ValueError:
+            print("\nChoose an Option")
 
     conn.commit()
     conn.close()
 
+def delete_entry():
+    conn = sqlite3.connect('contactBook.db')
+    global rows, c
+    c = conn.cursor()
+
+    running = True
+    while running:
+        try:
+            delete = int(input("DELETE ENTRY\nSearch by:\n1 - First Name\n2 - Last Name\n3 - Address\n" +
+                                   "4 - Contact Number\n5 - Nothing/Return"))
+            if delete == 1:
+                target = "first_name"
+                search = input("Insert the First Name of the Person that you want to Delete: ")
+                running = False
+            elif delete == 2:
+                target = "last_name"
+                search = input("Insert the Last Name of the Person that you want to Delete: ")
+                running = False
+            elif delete == 3:
+                target = "address"
+                search = input("Insert the Address of the Person that you want to Delete: ")
+                running = False
+            elif delete == 4:
+                target = "contact_number"
+                search = input("Insert the Contact's Number that you want to Delete: ")
+                running = False
+            elif delete == 5:
+                running = False
+                pass
+            else:
+                print("Invalid Option! Try Again\n")
+            sql = ('''DELETE FROM contacts WHERE "{0}" = "{1}"'''.format(target, search))
+            c.execute(sql)
+
+        except ValueError:
+            print("\nChoose an Option")
+
+
+    conn.commit()
+    conn.close()
 def menu():
+    create_database()
     print("Welcome to the Contact Book!")
     program_running = True
 
     while program_running:
-        choice = int(input("\n1 - Add a Contact\n2 - Consult\n3 - Update\n4 - Exit\n5 - Drop Table\nChoice: "))
+        try:
+            choice = int(input("\n1 - Add a Contact\n2 - Consult\n3 - Update\n4 - Delete\n5 - Exit\n6 - Drop Table\nChoice: "))
 
-        if choice == 1:
-            add_contact()
-        elif choice == 2:
-            search_contact()
-        elif choice == 3:
-            update_contact()
-        elif choice == 4:
-            exit()
-        elif choice == 5:
-            drop_table()
-        else:
-            print("\nInvalid Choice!")
+            if choice == 1:
+                add_contact()
+            elif choice == 2:
+                search_contact()
+            elif choice == 3:
+                update_contact()
+            elif choice == 4:
+                delete_entry()
+            elif choice == 5:
+                exit()
+            elif choice == 6:
+                drop_table()
+            else:
+                print("\nInvalid Choice!")
+        except ValueError:
+            print("\nChoose an Option")
 
 menu()
